7feac08c000acdbbaf4c11f8f11d554f079d2c7f
palomagfb <paloflobarr05@gmail.com>
2024-07-10 14:47:58 -0400

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,18 @@
+
+# calc.py
+# Este archivo puede contener funciones de cálculo, si es necesario.
+
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "División por cero no permitida"
---
+++ sample.py
@@ -0,0 +1,35 @@
+
+# sample.py
+def metodo1():
+    # Aquí va el código del método 1
+    comment = input("Ingrese un comentario: ")
+    post = f'ポスト: {comment}'
+    with open("data.txt", "w") as file_pc:
+        file_pc.write(post)
+    print("Evaluación completada")
+
+def metodo2():
+    # Aquí va el código del método 2
+    print('これまでの結果')
+    with open("data.txt", "r") as read_file:
+        print(read_file.read())
+
+def metodo3():
+    # Aquí va el código del método 3
+    print('終了します')
+    exit()
+
+def main():
+    while True:
+        num = int(input("Ingrese un número (1-3): "))
+        if num == 1:
+            metodo1()
+        elif num == 2:
+            metodo2()
+        elif num == 3:
+            metodo3()
+        else:
+            print('1から3で入力してください')
+
+if __name__ == "__main__":
+    main()
